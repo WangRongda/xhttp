99ee26e1963f6d29d8116a7ef209598aed11e823
rongda.wang <rongda.wang@broadlink.com.cn>
2018-03-22 12:51:14 +0800

    add proxy path ruler

--- http.go
+++ http.go
@@ -25,6 +25,9 @@ func (s Staticer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type Proxyer struct {
 	Addr string
 	Hook http.HandlerFunc
+	// default(reserve path): origin://proxy/user/login => target://proxy/user/login
+	// example: /proxy/user/login => /usr/login
+	PathRule func(string) string
 }
 
 func (p Proxyer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -38,21 +41,50 @@ func (p Proxyer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		p.Hook(w, r)
 	}
 
-	// proxy request
+	// proxy request(default)
+	endpoint := r.URL.String()
+	if nil != p.PathRule {
+		endpoint = p.PathRule(endpoint)
+	}
 	var err error
-	if r.URL, err = url.Parse(p.Addr + r.URL.String()); nil != err {
+	if r.URL, err = url.Parse(p.Addr + endpoint); nil != err {
 		log.Println(err)
 		return
 	}
-	r.RequestURI = ""
+	r.RequestURI = "" //should empty
+
+	//log
+	fmt.Println("Proxy ", r.URL.String())
+
 	client := &http.Client{}
 	var resp *http.Response
-	fmt.Println(r.URL)
+	blue := "\033[0;31m"
+	nc := "\033[0m"
 	if resp, err = client.Do(r); nil != err {
-		log.Println(err)
+		log.Printf(blue+"%v"+nc, err)
+
+		// resp == nil
 		w.WriteHeader(http.StatusServiceUnavailable)
 		return
 	}
 	// proxy response
+
+	for key, values := range resp.Header {
+		// for _, value := range values {
+		// 	log.Println(key, value)
+		// 	w.Header().Add(key, value)
+		// }
+		w.Header().Set(key, values[0])
+	}
+	// w.Header().Set("Content-Length", "5")
+	w.WriteHeader(resp.StatusCode)
+	// w.Write([]byte("hello"))
+	// w.Write([]byte("d"))
+	// go as(w, resp.Body)
 	io.Copy(w, resp.Body)
 }
+
+// func as(w io.Writer, r io.Reader) {
+// 	// io.Copy(w, r)
+// 	w.Write([]byte("helllllll"))
+// }
